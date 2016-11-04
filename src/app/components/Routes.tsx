cceb913ecb16b1bcdb05747362a334bdcf8e5fb0
Paul Krisadaolarn <pauly.k@gmail.com>
2016-11-04 12:42:10 +0700

    store.ts update npm packages

--- src/app/components/Routes.tsx
+++ src/app/components/Routes.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Provider } from 'react-redux'
+import store from '../state/store'
 
 import App from './App'
 import Home from './Home'
 import SigninPage from './Signin/SigninPage'
 
 export default (
+    <Provider store={store}>
     <Router history={browserHistory}>
         <Route path="/" component={App} >
             <IndexRoute component={Home} />
             <Route path="/signin" component={SigninPage} />
         </Route>
     </Router>
+    </Provider>
 )
